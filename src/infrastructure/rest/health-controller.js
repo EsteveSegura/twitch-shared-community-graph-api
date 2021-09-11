0fba14306a7c06f7ebd7c091854d02e777ed9c86
Esteve <estevesegura@protonmail.com>
2021-09-11 02:29:31 +0200

    :ant: small change in order to confirm CI is working

--- src/infrastructure/rest/health-controller.js
+++ src/infrastructure/rest/health-controller.js
@@ -5,7 +5,7 @@ const { server: { port } } = require('../config/');
 
 router.get('/', async (req, res) => {
     try {
-        res.status(200).send(`OK ${port}`)
+        res.status(200).send(`${port} OK`)
     } catch (error) {
         res.status(500).send("FAIL")
     }
